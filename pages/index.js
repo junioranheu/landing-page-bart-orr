7e5d421099535894e476c123cf22a064ee5c0a64
junioranheu <junioranheu@gmail.com>
2022-03-04 17:04:50 -0300

    início

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import styles from '../styles/index.module.css'
+import Image from 'next/image';
+import Logo from '../static/images/logo.webp';
+import ImagemUm from '../static/images/um.webp';
+import Styles from '../styles/index.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <b>Teste</b>
+    <div className={Styles.container}>
+      <div className={Styles.logo}>
+        <Image src={Logo} alt='' width={65} height={65} />
+      </div>
+
+      <div className={Styles.divCard}>
+        <Image className={Styles.card} src={ImagemUm} alt='' width={504} height={504} />
+      </div>
+
+      <div className={Styles.margemTexto}>
+        <span className={Styles.titulo}>OUT NOW</span>
+      </div>
     </div>
   )
 }
