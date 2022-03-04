c70be230e81e32174082debba5aa0fb698dd4927
junioranheu <junioranheu@gmail.com>
2022-03-04 17:34:37 -0300

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import { faAmazon, faApple, faElementor, faItunes, faPinterest, faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Logo from '../static/images/logo.webp';
 import ImagemUm from '../static/images/um.webp';
 import Styles from '../styles/index.module.css';
 
-export default function Home() {
+export default function Index() {
   return (
     <div className={Styles.container}>
       <div className={Styles.logo}>
@@ -17,6 +19,16 @@ export default function Home() {
       <div className={Styles.margemTexto}>
         <span className={Styles.titulo}>OUT NOW</span>
       </div>
+
+      <div className={Styles.margemPequena}>
+        <FontAwesomeIcon icon={faAmazon} className={Styles.icone} />
+        <FontAwesomeIcon icon={faApple} className={Styles.icone} />
+        <FontAwesomeIcon icon={faElementor} className={Styles.icone} />
+        <FontAwesomeIcon icon={faItunes} className={Styles.icone} />
+        <FontAwesomeIcon icon={faPinterest} className={Styles.icone} />
+        <FontAwesomeIcon icon={faSpotify} className={Styles.icone} />
+      </div>
     </div>
   )
 }
+
